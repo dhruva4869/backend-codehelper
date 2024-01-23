680b7e39dc95f9c7ae57a2cd912b051e9a83d179
dhruva4869 <101694640+dhruva4869@users.noreply.github.com>
2024-01-23 22:24:06 +0530

    Update connectDB.js

--- connectDB.js
+++ connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        const conn = await mongoose.connect(process.env."mongodb+srv://noobmaster69cr7:g9iIxLzZpg4H52za@cluster0.hfc269j.mongodb.net/Books");
         console.log(`Database Connected ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
